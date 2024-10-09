128b2ffde3af5063d7c3d0ab12654b0c8099fcc7
avarela67299 <alejovarela98@hotmail.com>
2024-10-09 07:32:15 -0700

    Oct 9

--- notas.js
+++ notas.js
@@ -165,3 +165,14 @@ function caseInSwitch(val) { // This fuction will return a greek letter dependin
   }
   return answer;
 }
+// this can be use in the case of an or statement
+let result = "";
+switch (val) {
+  case 1:
+  case 2:
+  case 3:
+    result = "1, 2, or 3";
+    break;
+  case 4:
+    result = "4 alone";
+}
